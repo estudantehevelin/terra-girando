c17a421bb660a5a4c0533f067b13948a9861520c
estudantehevelin <hevelin.sobral@escola.pr.gov.br>
2026-03-04 15:40:04 -0300

    esfera

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas (1920,1080,WEBGL); //PLANO 3D
+    terra=loadImage('terra')
+}
+
+function draw() {
+    brackground(205,102,94);//cor de fundo
+    rotateY(millis()/1000);//rotação da terra
+    Texture(terra); //textura da terra,imagem.
+    Sphere(80,100); //esfera 3D
+}
